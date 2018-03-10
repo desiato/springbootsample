861eb1abf216d2350c4d0ba14e71dabb255d62ec
Thomas Wieger <thomas.wieger@eng-softwarelabs.de>
2018-03-10 16:18:06 +0100

    added Date to hello world

--- src/main/java/org/sample/Sample.java
+++ src/main/java/org/sample/Sample.java
@@ -2,12 +2,13 @@ package org.sample;
 
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
+import java.util.Date;
 
 @RestController
 public class Sample {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World! v4";
+        return "Hello World! v4" + new Date();
     }
 }
